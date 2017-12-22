f566f51951ee9cc79a6923b6514e6227c5561df3
David Goodrich <djgoodrich15@gmail.com>
2017-12-22 10:25:00 -0500

    added mobile responsiveness to about me section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -12,7 +12,7 @@ const About = () => {
       <div className="about-grid">
         <div className="box1">
         <img src={problemsolver} className="icon-size" alt="problem solver icon"/>
-        <p className="text-center" id="about-paragraph">
+        <p className="text-center about-paragraph">
           Hi, I'm David and I love building beautiful, dynamic, responsive
           websites. Since my very first computer (Apple IIe), I have loved
           technology and with the advent of the web I have been able to use that
@@ -21,14 +21,14 @@ const About = () => {
         </div>
         <div className="box2">
         <img src={teamwork} className="icon-size" alt="teamwork"/>
-        <p className="text-center" id="about-paragraph">
+        <p className="text-center about-paragraph">
           I consider myself a great team player who is consistenly reliable, able to adapt quickly,
           contribute positively to the team, and can consider different points of view to accomplish a common goal.
         </p>
         </div>
         <div className="box3">
         <img src={browserImg} className="icon-size" alt="computer icon" />
-        <p className="text-center" id="about-paragraph">
+        <p className="text-center about-paragraph">
           I am a quick analytical, learner who loves to solve complex problems to create
           amazing solutions.
         </p>
